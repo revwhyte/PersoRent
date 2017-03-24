fd2a947c3b135921f32c65a023c34692e947336f
Philippe Correia <philippe.barbosa@ufba.br>
2017-03-24 19:52:02 -0300

    Diagramas

--- PersoRent/model/CNHModel.php
+++ PersoRent/model/CNHModel.php
@@ -12,7 +12,27 @@
         }
 
         public function criaCNH($dbh) {
-            
+            try {
+                $sth = $sbh->prepare("INSERT INTO cnh(numero, categoria, validade) VALUES(:numero, :categoria, :validade)");
+
+                $sth->bindParam(":numero", $this->numero, PDO::PARAM_INT);
+                $sth->bindParam(":categoria", $this->categoria, PDO::PARAM_INT);
+                $sth->bindParam(":validade", $this->validade, PDO::PARAM_INT);
+
+                return $sth->execute();
+            } catch(PDOException $e) {
+                $e->getMessage();
+            }
+        }
+
+        public static function buscaCNH($dbh, $dados) {
+            try {
+                $sth->prepare("SELECT numero, categoria, validade FROM cnh WHERE numero = :numero");
+
+                $sth->bindParam(":numero", $dados['numero'], PDO::PARAM_INT);
+            } catch(PDOException $e) {
+                $e->getMessage();
+            }
         }
     }
 ?>
